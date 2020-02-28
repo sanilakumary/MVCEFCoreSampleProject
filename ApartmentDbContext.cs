112698dcf6de443af7dd34513766536413b78061
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2020-02-28 02:01:37 +0100

    Add files via upload

---
+++ ApartmentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApartmentAdministrationNew.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApartmentAdministrationNew.DBContext
+{
+    public class ApartmentDbContext : DbContext
+    {
+        public ApartmentDbContext(DbContextOptions<ApartmentDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Apartment> apartments { get; set; }
+    }
+}
