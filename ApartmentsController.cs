ee50ad80003c752e26dc96e602015c739865f84e
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2020-02-28 02:01:18 +0100

    Add files via upload

---
+++ ApartmentsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApartmentAdministrationNew.DBContext;
+using ApartmentAdministrationNew.Models;
+
+namespace ApartmentAdministrationNew.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private readonly ApartmentDbContext _context;
+
+        public ApartmentsController(ApartmentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Apartments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.apartments.ToListAsync());
+        }
+
+        // GET: Apartments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.apartments
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+        public async Task<IActionResult> ActiveList(bool? active)
+        {
+            active = false;
+            if (active == null)
+            {
+                return NotFound();
+            }
+
+            var apartmentiist = await _context.apartments.Where(m => m.IsInActive == active).ToListAsync();
+            if (apartmentiist == null)
+            {
+                return NotFound();
+            }
+            return View(apartmentiist);
+           
+        }
+        public async Task<IActionResult> RecentList()
+        {
+            
+            return View(_context.apartments.Last());
+
+        }
+        // GET: Apartments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Street,Zipcode,City,IsInActive")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(apartment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+            return View(apartment);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Street,Zipcode,City,IsInActive")] Apartment apartment)
+        {
+            if (id != apartment.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(apartment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApartmentExists(apartment.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(apartment);
+        }
+       
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var apartment = await _context.apartments.FindAsync(id);
+            _context.apartments.Remove(apartment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApartmentExists(int id)
+        {
+            return _context.apartments.Any(e => e.ID == id);
+        }
+    }
+}
